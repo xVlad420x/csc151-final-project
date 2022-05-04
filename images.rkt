dfc28b061f552fef9092319c327d43dfdcd33da1
Kr1shn9 <104475738+Kr1shn9@users.noreply.github.com>
2022-05-03 22:19:02 -0500

    Add files via upload

--- images.rkt
+++ images.rkt
@@ -1,57 +1,63 @@
 #lang racket
 (require 2htdp/image)
 
-(define person
-  (lambda (skin-color shirt-color pants-color)
-    (above (circle 25 'solid skin-color)
-           (above (rectangle 140 20 'solid shirt-color)
-                  (rectangle 40 50 'solid shirt-color))
-           (rectangle 40 80 'solid pants-color))))
-
-
-
 (define sword
-  (above (triangle 30 'solid "silver")
-                     (rectangle 30 120 'solid "silver")
-                     (rectangle 50 20 'solid "black")
-                     (rectangle 10 30 'solid "black")))
+  (rotate -135 (above (triangle 30 'solid "silver")
+                      (rectangle 30 120 'solid "silver")
+                      (rectangle 50 20 'solid "black")
+                      (rectangle 10 30 'solid "black"))))
 (define spear
   (above (triangle 30 'solid "silver")
-                     (overlay/align 'center 'top (rotate 180 (triangle 30 'solid "silver"))
-                                    (rectangle 10 200 'solid "beige"))))
+         (overlay/align 'center 'top (rotate 180 (triangle 30 'solid "silver"))
+                        (rectangle 10 200 'solid "beige"))))
 (define bow
   (rotate -75 (wedge 60 150 'outline (pen "brown" 8 'solid 'projecting 'round))))
 
 (define choose-your-weapon
-   (beside sword spear bow))
+  (beside sword spear bow))
+
+(define person
+  (lambda (skin-color shirt-color pants-color weapon)
+    (let ([base (above (circle 25 'solid skin-color)
+                       (above (rectangle 140 20 'solid shirt-color)
+                              (rectangle 40 50 'solid shirt-color))
+                       (rectangle 40 80 'solid pants-color))])
+      (cond
+        [(equal? weapon sword)
+         (overlay base weapon)]
+        [(equal? weapon spear)
+         (beside/align 'bottom base weapon)]
+        [(equal? weapon bow)
+         (beside base weapon)]
+        [else
+         (error "invalid weapon!")]))))
 
 (define scene-1
-  (lambda (skin-color shirt-color pants-color)
-  (overlay/align 'center 'bottom (above (scale 0.5 (person skin-color shirt-color pants-color))
-                                                (rectangle 500 50 'solid "silver"))
-   (rectangle 500 300 'solid "black"))))
+  (lambda (skin-color shirt-color pants-color weapon)
+    (overlay/align 'center 'bottom (above (scale 0.5 (person skin-color shirt-color pants-color weapon))
+                                          (rectangle 500 50 'solid "silver"))
+                   (rectangle 500 300 'solid "black"))))
 
 (define level-0
   (overlay (above (rotate 120 (rhombus 90 60 'solid "black"))
                   (rotate -120 (rhombus 90 60 'solid "black"))
                   (rotate 120 (rhombus 90 60 'solid "black"))
                   (rotate -120 (rhombus 90 60 'solid "black")))
-  (rectangle 500 300 'solid "silver")))
+           (rectangle 500 300 'solid "silver")))
 
 (define level-0-lose
-  (lambda (skin-color shirt-color pants-color)
+  (lambda (skin-color shirt-color pants-color weapon)
     (overlay/align 'center 'top (above (text "A" 24 "white")
-                    (text "A" 24 "white")
-                    (text "A" 24 "white")
-                    (text "A" 24 "white")
-                    (text "A" 24 "white")
-                    (text "A" 24 "white")
-                    (text "A" 24 "white")
-                    (text "A" 24 "white")
-              (scale .25 (person skin-color shirt-color pants-color)))
-    (overlay (rectangle 200 300 'solid "black")
-             (rectangle 500 300 'solid "silver")))))
-
+                                       (text "A" 24 "white")
+                                       (text "A" 24 "white")
+                                       (text "A" 24 "white")
+                                       (text "A" 24 "white")
+                                       (text "A" 24 "white")
+                                       (text "A" 24 "white")
+                                       (text "A" 24 "white")
+                                       (scale .25 (person skin-color shirt-color pants-color)))
+                   (overlay (rectangle 200 300 'solid "black")
+                            (rectangle 500 300 'solid "silver")))))
 
 (define level-0-win
   (let* ([small-gray (circle 10 'solid "gray")]
@@ -61,7 +67,7 @@
          [warp-bottom-left (scale 3 (overlay/align 'left 'bottom small-gray medium-red large-black))]
          [warp-top-right (scale 3 (overlay/align 'right 'top small-gray medium-red large-black))]
          [warp-bottom-right (scale 3 (overlay/align 'right 'bottom small-gray medium-red large-black))])
-    (lambda (skin-color shirt-color pants-color)
+    (lambda (skin-color shirt-color pants-color weapon)
       (overlay (person skin-color shirt-color pants-color) (overlay/align 'right 'bottom warp-bottom-right
                                                                           (overlay/align 'right 'top warp-top-right
                                                                                          (overlay/align 'left 'bottom warp-bottom-left
